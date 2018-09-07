1e3a7399cc508667efa2f51c806f7e677cb7be4d
Sebastien Carlier <s.carlier@epixode.fr>
2018-09-07 08:50:44 +0000

    add Spinner component

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,8 @@
+
+import * as React from 'react';
+import {Spinner as BpSpinner} from '@blueprintjs/core';
+
+const Spinner : React.StatelessComponent<{}> = () =>
+  <BpSpinner size={BpSpinner.SIZE_SMALL} />;
+
+export default Spinner;
