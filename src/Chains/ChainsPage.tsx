9a5892d47fe0eef596b458a1f078382121c07825
Melanie Storup <m.storup@epixode.fr>
2018-09-14 22:34:01 +0200

    Chains: add stylesheet, filters

--- src/Chains/ChainsPage.tsx
+++ src/Chains/ChainsPage.tsx
@@ -1,6 +1,8 @@
 
 import * as React from 'react';
 import {connect} from 'react-redux';
+import {Button, InputGroup} from "@blueprintjs/core";
+//import {Select} from "@blueprintjs/select";
 
 import {State, DispatchProp} from '../app';
 import {Header as ContestHeader} from '../Contest';
@@ -28,14 +30,43 @@ function mapStateToProps (state: State, _props: object): StoreProps {
 class ChainsPage extends React.PureComponent<Props> {
   render () {
     const {loaded, chains} = this.props;
+    const searchIcon = <Button icon="search" minimal/>;
     return (
       <div>
         <ContestHeader/>
+        {!loaded && <Spinner/>}
+        <div className="chainFilters">
+          <div className="flexRow">
+            <div>
+              <div className="sectionTitle">Filter by Status</div>
+              <div className="chainStatus">
+                <Button text="Active" />
+                <Button text="Private test" />
+                <Button text="Public test" />
+                <Button text="Candidate" />
+                <Button text="Past" />
+              </div>
+            </div>
+            <div>
+              <div className="sectionTitle">Team</div>
+              <select>
+                <option>Team 1</option>
+                <option>Team 2</option>
+                <option>TODO</option>
+              </select>
+            </div>
+            <div>
+              <InputGroup type="search" placeholder="Search" rightElement={searchIcon} />
+            </div>
+            <div>
+              <Button text="Options" rightIcon="chevron-down" />
+            </div>
+          </div>
+        </div>
+        {chains && chains.map((chain, index) =>
+          <ChainItem chain={chain} key={index}/>)}
         <div className="pageContent chainsPage">
           <p>{"Everything goes here"}</p>
-          {!loaded && <Spinner/>}
-          {chains && chains.map(chain =>
-            <ChainItem chain={chain} />)}
         </div>
       </div>
     );
@@ -49,7 +80,12 @@ type ChainItemProps = {
 class ChainItem extends React.PureComponent<ChainItemProps> {
   render () {
     const {chain} = this.props;
-    return <Json value={chain} />;
+    return (
+      <div>
+        {chain.title}{" "}
+        <Json value={chain} />
+      </div>
+     );
   }
 }
 
