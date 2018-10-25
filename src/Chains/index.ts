dbc8e1fb28cc1d618d44b53257e2ec6f19644b79
Sebastien Carlier <s.carlier@epixode.fr>
2018-10-25 22:44:56 +0000

    subscriptions cleanup

--- src/Chains/index.ts
+++ src/Chains/index.ts
@@ -157,6 +157,7 @@ function* chainsPageSaga (params: Params): Saga {
         yield call(restartChain, chainId);
         const chain = yield select(getChain, chainId);
         yield call(loadChainGame, chain);
+        yield call(updateSubscriptions, params);
       });
     }
   );
@@ -231,7 +232,7 @@ function* chainListSaga (params: Params) : Saga {
     });
   });
   yield takeLatest(ActionTypes.CHAIN_LIST_SCROLLED, function* () : Saga {
-    yield call(updateSubscriptions);
+    yield call(updateSubscriptions, params);
   });
   yield takeLatest(ActionTypes.CHAIN_CREATED, function*(): Saga {
     yield call(refreshChainList, params.contestId);
@@ -242,35 +243,7 @@ function* chainListSaga (params: Params) : Saga {
       yield call(navigate, "ChainsPage", {contestId: params.contestId});
     }
   });
-  yield call(updateSubscriptions);
-  function* updateSubscriptions() {
-    const {visibleChains, visibleIds, newChains, selectedChain}: {visibleChains: Entity<Chain>[], visibleIds: string[], newChains: Entity<Chain>[], selectedChain: Entity<Chain>} =
-      yield select((state: State) => {
-        const {firstVisible, lastVisible, visibleIds: oldIds} = state.chainList;
-        const visibleIds = state.chainIds.slice(firstVisible, lastVisible + 1);
-        const newIds = difference(visibleIds, oldIds);
-        const visibleChains = visibleIds.map(id => selectors.getChain(state, id));
-        const newChains = newIds.map(id => selectors.getChain(state, id));
-        const selectedChain = selectors.getChain(state, params.chainId || null);
-        return {visibleChains, visibleIds, newChains, selectedChain};
-      });
-    /* Update subscriptions to the visible games plus the selected chain. */
-    const channels = [`contest:${params.contestId}`];
-    function addChainGameKey(chain: Entity<Chain>) {
-      if (chain.isLoaded && chain.value.currentGameKey !== "") {
-        channels.push(`game:${chain.value.currentGameKey}`)
-      }
-    }
-    for (let chain of visibleChains) {
-      visibleIds.push(chain.id);
-      addChainGameKey(chain);
-    }
-    addChainGameKey(selectedChain);
-    yield put(actionCreators.eventSourceSubsChanged(channels));
-    /* Load the games on the newly visible chains. */
-    yield call(loadChainGames, newChains);
-    yield put(actionCreators.chainListUpdateDone(visibleIds));
-  }
+  yield call(updateSubscriptions, params);
 }
 
 function* commonStartupSaga(contestId: string): Saga {
@@ -359,4 +332,34 @@ function getVisibleChains (state: State): Entity<Chain>[] {
 
 function getChain(state: State, chainId: string): Entity<Chain> {
   return selectors.getChain(state, chainId);
+}
+
+function* updateSubscriptions(params: Params): Saga {
+  const {visibleChains, visibleIds, newChains, selectedChain}: {visibleChains: Entity<Chain>[], visibleIds: string[], newChains: Entity<Chain>[], selectedChain: Entity<Chain>} =
+    yield select((state: State) => {
+      const {firstVisible, lastVisible, visibleIds: oldIds} = state.chainList;
+      const visibleIds = state.chainIds.slice(firstVisible, lastVisible + 1);
+      const newIds = difference(visibleIds, oldIds);
+      const visibleChains = visibleIds.map(id => selectors.getChain(state, id));
+      const newChains = newIds.map(id => selectors.getChain(state, id));
+      const selectedChain = selectors.getChain(state, params.chainId || null);
+      return {visibleChains, visibleIds, newChains, selectedChain};
+    });
+  /* Subscribe to the visible games plus that of the selected chain. */
+  const channels = new Set();
+  channels.add(`contest:${params.contestId}`);
+  function addChainGameKey(chain: Entity<Chain>) {
+    if (chain.isLoaded && chain.value.currentGameKey !== "") {
+      channels.add(`game:${chain.value.currentGameKey}`);
+    }
+  }
+  for (let chain of visibleChains) {
+    visibleIds.push(chain.id);
+    addChainGameKey(chain);
+  }
+  addChainGameKey(selectedChain);
+  yield put(actionCreators.eventSourceSubsChanged(Array.from(channels.keys())));
+  /* Load the games on the newly visible chains. */
+  yield call(loadChainGames, newChains);
+  yield put(actionCreators.chainListUpdateDone(visibleIds));
 }
