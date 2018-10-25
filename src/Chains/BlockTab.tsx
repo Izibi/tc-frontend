57e636de3e53e0f36e414bb4604d9871d8a5abc8
Melanie Storup <m.storup@epixode.fr>
2018-10-25 11:12:00 +0200

    blocks tab: style block rank table

--- src/Chains/BlockTab.tsx
+++ src/Chains/BlockTab.tsx
@@ -16,25 +16,30 @@ class BlockTab extends React.PureComponent<BlockTabProps> {
     }
     const viewUrl = `${process.env.BLOCKSTORE_URL}/${block.task}/view.html?h=${hash}`;
     return (
-      <div style={{display: 'flex'}}>
+      <div className="flexRow">
         <iframe style={{width: '400px', height: '400px', 'border': '0'}} src={viewUrl} />
-        <div>
-          {game && game.players &&
-            <table>
-              <thead>
-                <tr>
-                  <th>{"player"}</th>
-                  <th>{"team"}</th>
-                  <th>{"bot id"}</th>
-                  <th>{"score"}</th>
-                  <th>{"rank"}</th>
-                </tr>
-              </thead>
-              <tbody>
-                {game.players.map((player, i) => <PlayerRow key={player.rank} player={player} nbPlayers={game.nbPlayers}
-                  ranking={scores && scores.rankings[i]} />)}
-              </tbody>
-            </table>}
+        <div style={{margin: '0 10px', flex: '1 1 auto'}}>
+          <div className="panel">
+            <div className="panelHeader">{"Block ranking"}</div>
+            <div className="panelBody">
+              {game && game.players &&
+                <table>
+                  <thead>
+                    <tr>
+                      <th>{"player"}</th>
+                      <th>{"team"}</th>
+                      <th>{"bot id"}</th>
+                      <th>{"score"}</th>
+                      <th>{"rank"}</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {game.players.map((player, i) => <PlayerRow key={player.rank} player={player} nbPlayers={game.nbPlayers}
+                      ranking={scores && scores.rankings[i]} />)}
+                  </tbody>
+                </table>}
+            </div>
+          </div>
         </div>
       </div>
      );
@@ -51,7 +56,8 @@ function PlayerRow({player, nbPlayers, ranking}: PlayerRowProps) {
   const bgColor = `hsl(${hue}, 100%, 40%)`;
   return (
     <tr>
-      <td className='num' style={{backgroundColor: bgColor}}>{player.rank}</td>
+      <td className='num'>
+        <div className='playerColor' style={{backgroundColor: bgColor}}>{player.rank}</div></td>
       <td title={player.team.id}>
         {player.team.isLoaded && player.team.value.name}
       </td>
